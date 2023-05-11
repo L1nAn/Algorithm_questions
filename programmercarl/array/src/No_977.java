9059cf133dd39f03b57f6c8c593661f50f589b41
L1nAn <821720533@qq.com>
2023-05-11 17:29:04 +0800

    2023-5-11

---
+++ programmercarl/array/src/No_977.java
@@ -0,0 +1,34 @@
+/**
+ * @author :马治伟
+ * @version :1.0
+ * @Date : 2023/5/11
+ */
+public class No_977 {
+   public int[] sortedSquares(int[] nums) {
+      int left = 0;
+      int right = nums.length - 1;
+      //如果不创建新数组的话，就会弄来好多麻烦。
+      /**
+      while (left < right) {
+         if (nums[left] * nums[left] <= nums[right] * nums[right]) {
+            nums[right--] = nums[right] * nums[right];
+         } else if (nums[left] * nums[left] > nums[right] * nums[right]) {
+            nums[left++] = nums[left] * nums[left];
+         }
+      }
+       */
+      int[] result = new int[nums.length];
+      int index = result.length - 1;
+      while (left <= right) {
+         if (nums[left] * nums[left] <= nums[right] * nums[right]) {
+            result[index--] = nums[right] * nums[right];
+            right--;
+         } else if (nums[left] * nums[left] > nums[right] * nums[right]) {
+            result[index--] = nums[left] * nums[left];
+            left++;
+         }
+
+      }
+      return result;
+   }
+}
